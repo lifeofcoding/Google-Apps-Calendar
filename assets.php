b39043a8e109b5d9989a80bd8d0a409b6997aac0
Jimmy Rousseau <lifeofcoding@gmail.com>
2015-02-08 00:13:05 -0500

    Fixed asset.php formatting

--- assets.php
+++ assets.php
@@ -1,32 +1,32 @@
 <?php
-		class Asset
-		{
-			public $folder;
-			public $ext;
-			public $id;
-		}
+	class Asset
+	{
+		public $folder;
+		public $ext;
+		public $id;
+	}
 
-		$controllers = new Asset();
-		$controllers->folder = 'js/controllers';
-		$controllers->ext = 'js';
-		$controllers->id = false;
+	$controllers = new Asset();
+	$controllers->folder = 'js/controllers';
+	$controllers->ext = 'js';
+	$controllers->id = false;
 
-		$moduals = new Asset();
-		$moduals->folder = 'js/modules';
-		$moduals->ext = 'js';
-		$moduals->id = false;
+	$moduals = new Asset();
+	$moduals->folder = 'js/modules';
+	$moduals->ext = 'js';
+	$moduals->id = false;
 
-		$models = new Asset();
-		$models->folder = 'js/models';
-		$models->ext = 'js';
-		$models->id = false;
+	$models = new Asset();
+	$models->folder = 'js/models';
+	$models->ext = 'js';
+	$models->id = false;
 
-		$views = new Asset();
-		$views->folder = 'js/views';
-		$views->ext = 'js';
-		$views->id = false;
+	$views = new Asset();
+	$views->folder = 'js/views';
+	$views->ext = 'js';
+	$views->id = false;
 
-		$assetList = array($models, $views, $moduals, $controllers);
+	$assetList = array($models, $views, $moduals, $controllers);
 
 	foreach($assetList as $asset) {
 		if ($handle = opendir('./'.$asset->folder)) {
